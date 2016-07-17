4d7b62d9d3f13724a1f42bf8928eccdba0c92988
paul <pavopax@gmail.com>
2016-07-17 14:53:24 -0400

    [upload] use flask upload file code

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 # initialization
 app = Flask(__name__)
@@ -7,22 +11,40 @@ app.config.update(
     DEBUG = True,
 )
 
-# controllers
-# @app.route("/")
-# def hello():
-#     return "Hello from Python!"
 
-@app.route('/')
-# #@app.route('/', methods=['POST', 'GET'])
-def index():
-    # if request.method == 'POST':
-    #     uu = Upload()
-    #     for inputName in request.files:
-    #         for upload in request.files.getlist(inputName):
-    #             app.logger.debug(secure_filename(upload.filename))
-    #             upload.save(os.path.join('/tmp/demo', secure_filename(upload.filename)))
-    return render_template('templates/index.html')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join('/tmp', filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 
 # launch
