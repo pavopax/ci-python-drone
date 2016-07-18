67a1a11b2ade18ece307dedca9fb3c1064758b3c
paul <pavopax@gmail.com>
2016-07-18 13:41:19 -0400

    [tests] finalize build script

--- droneio/deploy-tests.sh
+++ droneio/deploy-tests.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+echo "Deploying drone..."
+
 touch drone.sqlite
 
 docker run -d --name="drone-ci" -p 8080:8080 -v /var/lib/drone/ -v /var/run/docker.sock:/var/run/docker.sock -v /root/droneio/drone.sqlite:/var/lib/drone/drone.sqlite my_drone
+
+echo "DONE. Test with <docker ps>"
