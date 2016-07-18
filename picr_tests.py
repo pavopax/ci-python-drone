d4cc9a2aea9042e7a1fe4bd8fb6b1b3d33b6cd26
paul <pavopax@gmail.com>
2016-07-17 20:53:49 -0400

    [test] sample test WORKS

--- picr_tests.py
+++ picr_tests.py
@@ -1,5 +1,5 @@
 import os
-import picr                     # my picr.py file
+import picr
 import unittest
 import tempfile
 
@@ -9,11 +9,14 @@ class PicrTestCase(unittest.TestCase):
         self.db_fd, picr.app.config['DATABASE'] = tempfile.mkstemp()
         picr.app.config['TESTING'] = True
         self.app = picr.app.test_client()
-        picr.init_db()
 
     def tearDown(self):
         os.close(self.db_fd)
         os.unlink(picr.app.config['DATABASE'])
+
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert 'Upload new File' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
