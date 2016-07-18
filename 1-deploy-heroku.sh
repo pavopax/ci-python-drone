b0521cba5d0ee12ffcec5afa5f294368730fc81e
paul <pavopax@gmail.com>
2016-07-18 11:16:39 -0400

    [script] add script to deploy heroku

---
+++ 1-deploy-heroku.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Deploying to heroku. Follow the commands..."
+
+cd picr
+
+git init
+
+git add *
+
+heroku login
+
+heroku create
+
+git push heroku master
+
+heroku open
+
+echo "DONE"
